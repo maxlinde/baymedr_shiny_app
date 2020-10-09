1a9fec9f8e9a411bda3cf07bfbb0873202365b25
maxlinde <maximilian.linde.92@gmail.com>
2020-10-10 01:16:15 +0200

    Make basic UI structure.

--- app.R
+++ app.R
@@ -4,7 +4,42 @@ library("shinythemes")
 
 # user interface
 ui <- fluidPage(
-   theme = shinytheme(theme = "cosmo") 
+    theme = shinytheme(
+        theme = "cosmo"
+    ),
+    navbarPage(
+        title = paste0("baymedr | v", packageVersion(pkg = "baymedr")),
+        tabPanel(
+            title = "Welcome"
+        ),
+        navbarMenu(
+            title = "Superiority",
+            tabPanel(
+                title = "Raw data"
+            ),
+            tabPanel(
+                title = "Summary data"
+            )
+        ),
+        navbarMenu(
+            title = "Non-inferiority",
+            tabPanel(
+                title = "Raw data"
+            ),
+            tabPanel(
+                title = "Summary data"
+            )
+        ),
+        navbarMenu(
+            title = "Equivalence",
+            tabPanel(
+                title = "Raw data"
+            ),
+            tabPanel(
+                title = "Summary data"
+            )
+        )
+    )
 )
 
 # server
