f2ca755c2c2abde4b94459e755b99ec0406bf57a
maxlinde <maximilian.linde.92@gmail.com>
2020-10-15 13:45:17 +0200

    Add functions for creating inputs in UI.

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,52 @@
-prior_scale <- function(id) {
-    textInput(
-        inputId = id,
-        label = "Scale parameter of the Cauchy prior",
-        value = "1 / sqrt(2)",
-        placeholder = "e.g., '1' or '1 / sqrt(2)'"
+general_ui <- function(id) {
+    name <- str_split(
+        string = id,
+        pattern = "-"
+    ) %>%
+        unlist() %>%
+        last()
+    switch(
+        name,
+        # prior_scale
+        prior_scale = textInput(
+            inputId = id,
+            label = "Scale parameter of the Cauchy prior",
+            value = "1 / sqrt(2)",
+            placeholder = "e.g., '1' or '1 / sqrt(2)'"
+        )
+    )
+}
+
+equiv_ui <- function(id) {
+    name <- str_split(
+        string = id,
+        pattern = "-"
+    ) %>%
+        unlist() %>%
+        last()
+    switch(
+        name,
+        # interval_low
+        interval_low = numericInput(
+            inputId = id,
+            label = "Lower boundary of the equivalece interval",
+            value = NULL
+        ),
+        # interval_high
+        interval_high = numericInput(
+            inputId = id,
+            label = "Upper boundary of the equivalece interval",
+            value = NULL
+        ),
+        # interval_std
+        interval_std = radioButtons(
+            inputId = id,
+            label = "Unit for the equivalence interval",
+            choices = setNames(
+                object = c("TRUE", "FALSE"),
+                nm = c("Standardised", "Unstandardised")
+            ),
+            selected = "TRUE"
+        )
     )
 }
