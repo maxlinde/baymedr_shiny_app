7b826ee0e0943d7d9c8b48e503a68488b3ba479d
maxlinde <maximilian.linde.92@gmail.com>
2020-10-16 12:39:22 +0200

    Add functions that allow for printing the results.

---
+++ R/results_show.R
@@ -0,0 +1,100 @@
+results_show <- function(id, ...) {
+    name <- str_sub(
+        string = id,
+        start = 1,
+        end = 5
+    )
+    switch(
+        name,
+        equiv = do.call(
+            what = equiv_show,
+            args = ...
+        )
+    )
+}
+
+equiv_show <- function(n_x = n_x,
+                       n_y = n_y,
+                       t = t_stat,
+                       test = test,
+                       hypotheses = hypotheses,
+                       interval = interval,
+                       prior_scale = prior_scale,
+                       bf = bf) {
+    cat(
+        strrep(
+            x = "*",
+            times = 30
+        ),
+        "\n",
+        test,
+        "\n",
+        strrep(
+            x = "-",
+            times = nchar(test)
+        ),
+        "\n",
+        "H0 (equivalence):             ",
+        hypotheses$h0,
+        "\n",
+        "H1 (non-equivalence):         ",
+        hypotheses$h1,
+        "\n",
+        "Equivalence interval:          Lower = ",
+        formatC(
+            x = interval$interval_low_std,
+            digits = 2,
+            format = "f"
+        ),
+        "; Upper = ",
+        formatC(
+            x = interval$interval_high_std,
+            digits = 2,
+            format = "f"
+        ),
+        " (standardised)",
+        "\n",
+        "                               Lower = ",
+        formatC(
+            x = interval$interval_low_unstd,
+            digits = 2,
+            format = "f"
+        ),
+        "; Upper = ",
+        formatC(
+            x = interval$interval_high_unstd,
+            digits = 2,
+            format = "f"
+        ),
+        " (unstandardised)",
+        "\n",
+        "Cauchy prior scale:           ",
+        formatC(
+            x = prior_scale,
+            digits = 3,
+            format = "f"
+        ),
+        "\n",
+        "\n",
+        "    BF01 (equivalence) = ",
+        if (bf > 1 / 1000 & bf < 1000) {
+            formatC(
+                x = bf,
+                digits = 3,
+                format = "f"
+            )
+        } else {
+            formatC(
+                x = bf,
+                digits = 3,
+                format = "e"
+            )
+        },
+        "\n",
+        strrep(
+            x = "*",
+            times = 30
+        ),
+        "\n"
+    )
+}
