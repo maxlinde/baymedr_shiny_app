dc3b75e9649d3e60a17ab69d5b8ddb7ef91e20e4
maxlinde <maximilian.linde.92@gmail.com>
2020-10-20 23:14:10 +0200

    Remove placeholder because it is redundant.

--- R/utils.R
+++ R/utils.R
@@ -11,8 +11,7 @@ general_ui <- function(id) {
         prior_scale = textInput(
             inputId = id,
             label = "Scale parameter of the Cauchy prior",
-            value = "1 / sqrt(2)",
-            placeholder = "e.g., '1' or '1 / sqrt(2)'"
+            value = "1 / sqrt(2)"
         ),
         # submit
         submit = actionButton(
