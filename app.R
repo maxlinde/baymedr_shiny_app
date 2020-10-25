7955918a3ab90d9a2fc223c87b4353925aef5b19
maxlinde <maximilian.linde.92@gmail.com>
2020-10-25 22:22:55 +0100

    Add structural comments.

--- app.R
+++ app.R
@@ -16,43 +16,56 @@ library("shinyhelper")
 
 # user interface
 ui <- fluidPage(
+    # theme
     theme = shinytheme(
         theme = "cosmo"
     ),
+    # navigation bar
     navbarPage(
+        # title
         title = paste0("baymedr | v", packageVersion(pkg = "baymedr")),
+        # welcome tab
         tabPanel(
             title = "Welcome",
             uiOutput("welcome")
         ),
+        # superiority menu
         navbarMenu(
             title = "Superiority",
+            # raw data
             tabPanel(
                 title = "Raw data",
                 superRawUi("superRaw")
             ),
+            # summary data
             tabPanel(
                 title = "Summary data",
                 superSummaryUi("superSummary")
             )
         ),
+        # non-inferiority menu
         navbarMenu(
             title = "Non-inferiority",
+            # raw data
             tabPanel(
                 title = "Raw data",
                 inferRawUi("inferRaw")
             ),
+            # summary data
             tabPanel(
                 title = "Summary data",
                 inferSummaryUi("inferSummary")
             )
         ),
+        # equivalence menu
         navbarMenu(
             title = "Equivalence",
+            # raw data
             tabPanel(
                 title = "Raw data",
                 equivRawUi("equivRaw")
             ),
+            # summary data
             tabPanel(
                 title = "Summary data",
                 equivSummaryUi("equivSummary")
