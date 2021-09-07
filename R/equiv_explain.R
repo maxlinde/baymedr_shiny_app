c638f05c1de317838d93b65fff87130ea9e58d4b
maxlinde <maximilian.linde.92@gmail.com>
2021-09-07 11:39:18 +0200

    Function that creates string of explanations for equivalence design.

---
+++ R/equiv_explain.R
@@ -0,0 +1,14 @@
+equiv_explain <- function(n_x = n_x,
+                          n_y = n_y,
+                          t = t_stat,
+                          test = test,
+                          hypotheses = hypotheses,
+                          interval = interval,
+                          prior_scale = prior_scale,
+                          bf = bf) {
+    if (interval$interval_low_std == interval$interval_high_std) {
+        "The plot shows the prior (dotted line) and posterior (solid line) for the population effect size. The Bayes factor is the ratio of the density of the posterior when the population effect size is zero (green dot) and the density of the prior when the population effect size is zero (purple dot)."
+    } else {
+        "The plot shows the prior (dotted line) and posterior (solid line) for the population effect size. The two vertical dashed lines demarcate the equivalence interval. The Bayes factor is the ratio of the posterior odds and the prior odds. The posterior odds is the ratio of the area inside the equivalence interval under the posterior (yellow area) and the area outside the equivalence interval under the posterior (green area). The prior odds is the ratio of the area inside the equivalence interval under the prior (blue area) and the area outside the equivalence interval under the prior (purple area)."
+    }
+}
