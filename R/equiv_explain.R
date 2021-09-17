000ab3b97a2906e18861de7d0a6f80bbcb64e75f
maxlinde <maximilian.linde.92@gmail.com>
2021-09-17 11:29:38 +0200

    Update explanation text for equivalence design.

--- R/equiv_explain.R
+++ R/equiv_explain.R
@@ -7,8 +7,8 @@ equiv_explain <- function(n_x = n_x,
                           prior_scale = prior_scale,
                           bf = bf) {
     if (interval$interval_low_std == interval$interval_high_std) {
-        "The plot shows the prior (dotted line) and posterior (solid line) for the population effect size. The Bayes factor is the ratio of the density of the posterior when the population effect size is zero (green dot) and the density of the prior when the population effect size is zero (purple dot)."
+        "The plot shows the prior (dotted line) and posterior (solid line) for the population effect size. The Bayes factor (BF01) is the ratio of the density of the posterior at a population effect size of zero (green dot) and the density of the prior at a population effect size of zero (purple dot)."
     } else {
-        "The plot shows the prior (dotted line) and posterior (solid line) for the population effect size. The two vertical dashed lines demarcate the equivalence interval. The Bayes factor is the ratio of the posterior odds and the prior odds. The posterior odds is the ratio of the area inside the equivalence interval under the posterior (yellow area) and the area outside the equivalence interval under the posterior (green area). The prior odds is the ratio of the area inside the equivalence interval under the prior (blue area) and the area outside the equivalence interval under the prior (purple area)."
+        "The plot shows the prior (dotted line) and posterior (solid line) for the population effect size. The two vertical dashed lines demarcate the equivalence interval. The Bayes factor (BF01) is the ratio of the posterior odds and the prior odds. The posterior odds is the ratio of the area inside the equivalence interval under the posterior (yellow area) and the area outside the equivalence interval under the posterior (green area). The prior odds is the ratio of the area inside the equivalence interval under the prior (blue area) and the area outside the equivalence interval under the prior (purple area)."
     }
 }
